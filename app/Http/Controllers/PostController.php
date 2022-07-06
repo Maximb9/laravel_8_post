c7b5bdbd1d2a0fa36502a49dd4ea91f82080b89a
Maxim <bmw_077@mail.ru>
2022-07-06 22:05:08 +0300

    belongs to many

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,18 +4,21 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use App\Models\Post;
+use App\Models\Tag;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index()
     {
+//        $category = Category::find(1);
+//
+//        dd($category->posts);
 
         $post = Post::find(1);
+        $tag = Tag::find(1);
 
-        dd($post->category);
-
-//        return view('post.index', compact('posts'));
+        dd($tag->posts);
     }
 
     public function create()
