787dcf760801e203830595184194b942a1f8c232
Maxim <bmw_077@mail.ru>
2022-07-09 21:38:33 +0300

    services

--- app/Http/Controllers/Post/StoreController.php
+++ app/Http/Controllers/Post/StoreController.php
@@ -7,22 +7,13 @@ use App\Http\Requests\Post\StoreRequest;
 use App\Models\Post;
 use Illuminate\Session\Store;
 
-class StoreController extends Controller
+class StoreController extends BaseController
 {
     public function __invoke(StoreRequest $request)
     {
         $data = $request->validated();
 
-        $tags = $data['tags'];
-        unset($data['tags']);
-
-
-        $post = Post::create($data);
-//        возьмем $post у него есть tags... Переходим в Models Post, у него есть метод tags()
-//        и есть соеденительная таблица post_tags (pivot) и в этой таблице привяжи вот к этому посту
-//        привяжи теги. Какие теги мы укажим здесь (принимает массив [] поэтому можем указать $tags).
-//        метод attach связывате посты и теги.
-        $post->tags()->attach($tags);
+        $this->service->store($data);
 
         return redirect()->route('post.index');
     }
--- app/Http/Controllers/Post/UpdateController.php
+++ app/Http/Controllers/Post/UpdateController.php
@@ -6,18 +6,12 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Post\UpdateRequest;
 use App\Models\Post;
 
-class UpdateController extends Controller
+class UpdateController extends BaseController
 {
     public function __invoke(UpdateRequest $request, Post $post)
     {
         $data = $request->validated();
-
-        $tags = $data['tags'];
-        unset($data['tags']);
-
-        $post->update($data);
-// метод sync удаляет и добавляет новые... обновляет, те которые есть не трогает, а добавляет новые.
-        $post->tags()->sync($tags);
+        $this->service->update($post, $data);
         return redirect()->route('post.show', $post->id);
     }
 }
