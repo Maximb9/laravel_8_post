726a12c90dcbd512c4c237f483c824f04dda05df
Maxim <bmw_077@mail.ru>
2022-07-24 21:19:21 +0300

    class resource

--- app/Http/Controllers/Post/StoreController.php
+++ app/Http/Controllers/Post/StoreController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Post;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Post\StoreRequest;
+use App\Http\Resources\Post\PostResource;
 use App\Models\Post;
 use Illuminate\Session\Store;
 
@@ -13,8 +14,10 @@ class StoreController extends BaseController
     {
         $data = $request->validated();
 
-        $this->service->store($data);
+        $post = $this->service->store($data);
 
-        return redirect()->route('post.index');
+        return new PostResource($post);
+
+//        return redirect()->route('post.index');
     }
 }
